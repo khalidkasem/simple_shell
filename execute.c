dabf2e2e94fea0cbfca4c79064322cc7271d0dd1
khalidkasem <khalidkasem1411@gmail.com>
2023-11-15 18:03:26 +0000

    execute.c

---
+++ execute.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * execute_proc - similar to puts in C
+ * @cmd_k1: a pointer the integer we want to set to 402
+ *
+ * Return: int
+ */
+void execute_proc(char **cmd_k1)
+{
+
+	char *parametro_k1 = (*(cmd_k1 + 1));
+	char *s_k1;
+	char *slash_k1 = "/";
+	char *o;
+
+	char *vartoprint = *cmd_k1;
+	char *argv_k1[4];
+
+	if ((access(cmd_k1[0], F_OK) == 0))
+	{
+		argv_k1[0] = cmd_k1[0];
+		argv_k1[1] = parametro_k1;
+		argv_k1[2] = ".";
+		argv_k1[3] = NULL;
+
+		if (execve(argv_k1[0], argv_k1, NULL) == -1)
+		{
+			perror("Error");
+		}
+	}
+	else
+	{
+		o = find_command(vartoprint);
+
+		slash_k1 = str_concat(o, slash_k1);
+
+		s_k1 = str_concat(slash_k1, *cmd_k1);
+
+		argv_k1[0] = s_k1;
+		argv_k1[1] = parametro_k1;
+		argv_k1[2] = ".";
+		argv_k1[3] = NULL;
+
+		if (execve(argv_k1[0], argv_k1, NULL) == -1)
+		{
+			perror("Error");
+		}
+	}
+}
